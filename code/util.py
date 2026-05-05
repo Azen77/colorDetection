84b226914a927983ff9a8a094ae43305ad0a444f
Azen77 <mazen.rashrash8000@gmail.com>
2026-05-05 14:50:11 -0400

    1

---
+++ code/util.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+def getlimits(color):
+    c = np.uint8([[color]]) # color is bgr values that you want in hsv
+    hsvC = cv2.cvtColor(c,cv2.COLOR_BGR2HSV)
+    
+    lowerLimit = hsvC[0][0][0] - 10,100,100
+    upperLimit = hsvC[0][0][0] + 10,255,255
+    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+    upperLimit = np.array(upperLimit, dtype=np.uint8)
+    
+    return lowerLimit, upperLimit
